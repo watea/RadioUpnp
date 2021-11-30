87811cb70da452891d9296a44243a8aa1513fc2f
watea <fr.watea@gmail.com>
2021-11-30 19:45:38 +0100

    Bassdrive radio.

--- app/src/main/java/com/watea/radio_upnp/activity/MainActivity.java
+++ app/src/main/java/com/watea/radio_upnp/activity/MainActivity.java
@@ -157,7 +157,12 @@ public class MainActivity
       "DAVIDE",
       R.drawable.logo_davide,
       "https://streaming01.zfast.co.uk/proxy/davideof",
-      "http://www.davideofmimic.com/")
+      "http://www.davideofmimic.com/"),
+    new DefaultRadio(
+      "BASSDRIVE",
+      R.drawable.logo_bassdrive,
+      "http://ice.bassdrive.net:80/stream",
+      "http://www.bassdrive.com/v2/")
   };
   private final NetworkProxy networkProxy = new NetworkProxy(this);
   // <HMI assets
