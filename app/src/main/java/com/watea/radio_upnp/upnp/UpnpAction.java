0439dc5a38360345a3244244f32ebc7aca2eec70
watea <fr.watea@gmail.com>
2024-08-05 23:32:01 +0100

    Copyright.

--- app/src/main/java/com/watea/radio_upnp/upnp/UpnpAction.java
+++ app/src/main/java/com/watea/radio_upnp/upnp/UpnpAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018. Stephane Treuchot
+ * Copyright (c) 2018-2024. Stephane Treuchot
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
