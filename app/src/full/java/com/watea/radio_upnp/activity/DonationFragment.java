9a2be5520ce597ecd1e7240789e44509863beb04
watea <fr.watea@gmail.com>
2025-07-16 14:59:54 +0100

    New billing library. V2.71.61.

--- app/src/full/java/com/watea/radio_upnp/activity/DonationFragment.java
+++ app/src/full/java/com/watea/radio_upnp/activity/DonationFragment.java
@@ -28,12 +28,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingClientStateListener;
@@ -43,6 +41,7 @@ import com.android.billingclient.api.ConsumeParams;
 import com.android.billingclient.api.ConsumeResponseListener;
 import com.android.billingclient.api.PendingPurchasesParams;
 import com.android.billingclient.api.ProductDetails;
+import com.android.billingclient.api.Purchase;
 import com.android.billingclient.api.PurchasesUpdatedListener;
 import com.android.billingclient.api.QueryProductDetailsParams;
 import com.google.common.collect.ImmutableList;
@@ -52,185 +51,132 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public class DonationFragment
-  extends OpenDonationFragment
+public class DonationFragment extends OpenDonationFragment
   implements ConsumeResponseListener, PurchasesUpdatedListener {
   private static final String LOG_TAG = DonationFragment.class.getSimpleName();
-  private static final long RECONNECT_TIMER_START_MILLISECONDS = 1000L; // 1s
-  private static final long RECONNECT_TIMER_MAX_TIME_MILLISECONDS = 1000L * 60L * 15L; // 15 mins
+  private static final long RECONNECT_TIMER_START_MILLISECONDS = 1000L;
   private static final Handler handler = new Handler(Looper.getMainLooper());
   private final Map<String, ProductDetails> ownProductDetailss = new HashMap<>();
-  private List<QueryProductDetailsParams.Product> googleProducts;
   private BillingClient billingClient;
-  private Spinner googleSpinner;
-  private AlertDialog.Builder paymentAlertDialogBuilder;
-
-  @NonNull
-  private static QueryProductDetailsParams.Product getProduct(@NonNull String name) {
-    return QueryProductDetailsParams.Product.newBuilder()
-      .setProductId(name)
-      .setProductType(BillingClient.ProductType.INAPP)
-      .build();
-  }
+  private String[] productIds;
 
   @Override
-  public void onConsumeResponse(@NonNull BillingResult billingResult, @NonNull String s) {
-    logBillingResult("onConsumeResponse", billingResult);
-    Log.d(LOG_TAG, "End consumption flow: " + s);
-  }
-
-  @Override
-  public void onPurchasesUpdated(
-    @NonNull BillingResult billingResult,
-    @Nullable List<com.android.billingclient.api.Purchase> purchases) {
-    logBillingResult("onPurchasesUpdated", billingResult);
-    switch (billingResult.getResponseCode()) {
-      case BillingClient.BillingResponseCode.OK:
-        // Consume purchase
-        if (purchases == null) {
-          Log.e(LOG_TAG, "onPurchasesUpdated: purchase list is null");
-        } else {
-          for (com.android.billingclient.api.Purchase purchase : purchases) {
-            Log.d(LOG_TAG, "onPurchasesUpdated: " + purchase);
-            billingClient.consumeAsync(
-              ConsumeParams.newBuilder().setPurchaseToken(purchase.getPurchaseToken()).build(),
-              this);
-          }
-        }
-        break;
-      case BillingClient.BillingResponseCode.USER_CANCELED:
-        // Nothing to do
-        break;
-      default:
-        paymentAlertDialogBuilder.show();
-    }
-  }
-
-  @Override
-  public void onCreateView(@NonNull View view, @Nullable ViewGroup container) {
-    super.onCreateView(view, container);
+  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    // Resource for Products Ids
+    productIds = getResources().getStringArray(R.array.google_products);
     // Visibility
     view.findViewById(R.id.google_charges_text_view).setVisibility(View.VISIBLE);
     view.findViewById(R.id.google_pay_image_button).setVisibility(View.VISIBLE);
     view.findViewById(R.id.google_or_use_text_view).setVisibility(View.VISIBLE);
     // Choose donation amount
-    googleSpinner = view.findViewById(R.id.donation_google_android_market_spinner);
+    final Spinner googleSpinner = view.findViewById(R.id.donation_google_android_market_spinner);
     googleSpinner.setVisibility(View.VISIBLE);
     // Buttons and listeners
     view.findViewById(R.id.google_pay_image_button).setOnClickListener(buttonView -> {
       if (ownProductDetailss.isEmpty() || !billingClient.isReady()) {
-        paymentAlertDialogBuilder.show();
+        tell(R.string.donation_error);
       } else {
-        final String productName =
-          googleProducts.get(googleSpinner.getSelectedItemPosition()).zza();
-        Log.d(LOG_TAG, "Selected item in spinner: " + productName);
-        final ProductDetails productDetails = ownProductDetailss.get(productName);
-        assert productDetails != null;
-        assert getActivity() != null;
-        billingClient.launchBillingFlow(
-          getActivity(),
-          BillingFlowParams.newBuilder()
+        final ProductDetails productDetails = ownProductDetailss.get(productIds[googleSpinner.getSelectedItemPosition()]);
+        if ((productDetails == null) || (productDetails.getOneTimePurchaseOfferDetails() == null)) {
+          tell(R.string.donation_error);
+        } else {
+          final BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
             .setProductDetailsParamsList(ImmutableList.of(
               BillingFlowParams.ProductDetailsParams.newBuilder()
                 .setProductDetails(productDetails)
-                .build()))
-            .build());
+                .build()
+            ))
+            .build();
+          billingClient.launchBillingFlow(requireActivity(), billingFlowParams);
+        }
       }
     });
-    // Alert dialog
-    assert getContext() != null;
-    paymentAlertDialogBuilder = new AlertDialog.Builder(getContext())
-      .setIcon(android.R.drawable.ic_dialog_alert)
-      .setTitle(R.string.donation_alert_dialog_title)
-      .setMessage(R.string.donation_alert_dialog_try_again)
-      .setCancelable(true)
-      .setNeutralButton(R.string.donation_button_close, (dialog, which) -> dialog.dismiss());
+    initGoogleBillingClient();
   }
 
   @Override
-  public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // Google products
-    googleProducts = Arrays.stream(getResources().getStringArray(R.array.google_products))
-      .map(DonationFragment::getProduct)
-      .collect(Collectors.toList());
-    // BillingClient, new each time
-    assert getContext() != null;
-    billingClient = BillingClient.newBuilder(getContext())
-      .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
+  public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> purchases) {
+    if ((billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) && (purchases != null)) {
+      for (Purchase purchase : purchases) {
+        handlePurchase(purchase);
+      }
+    } else if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED) {
+      Log.i(LOG_TAG, "Purchase canceled by user");
+    } else {
+      Log.e(LOG_TAG, "Purchase failed: " + billingResult.getDebugMessage());
+    }
+  }
+
+  @Override
+  public void onConsumeResponse(@NonNull BillingResult billingResult, @NonNull String purchaseToken) {
+    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+      Log.i(LOG_TAG, "Donation consumed, thank you!");
+    } else {
+      Log.e(LOG_TAG, "Consumption failed: " + billingResult.getDebugMessage());
+    }
+  }
+
+  private void initGoogleBillingClient() {
+    billingClient = BillingClient.newBuilder(requireContext())
       .setListener(this)
+      .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
       .build();
     billingClient.startConnection(new BillingClientStateListener() {
-      private long reconnectMilliseconds = RECONNECT_TIMER_START_MILLISECONDS;
-
-      @Override
-      public void onBillingServiceDisconnected() {
-        ownProductDetailss.clear();
-        retryBillingServiceConnectionWithExponentialBackoff();
-      }
-
       @Override
       public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
-        logBillingResult("onBillingSetupFinished", billingResult);
         if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-          // The billing client is ready. You can query purchases here.
-          // This doesn't mean that your app is set up correctly in the console -- it just
-          // means that you have a connection to the Billing service.
-          reconnectMilliseconds = RECONNECT_TIMER_START_MILLISECONDS;
-          // Query product details asynchronously
-          billingClient.queryProductDetailsAsync(
-            QueryProductDetailsParams.newBuilder().setProductList(googleProducts).build(),
-            (billingClientResult, productDetailss) -> {
-              logBillingResult("productDetailsResponseListener", billingClientResult);
-              if (billingClientResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                if (productDetailss.isEmpty()) {
-                  Log.e(LOG_TAG, "productDetailsResponseListener: null or empty response." +
-                    "Check to see if the billing you requested are correctly published " +
-                    "in the Google Play Console.");
-                } else {
-                  for (ProductDetails productDetails : productDetailss) {
-                    ownProductDetailss.put(productDetails.getProductId(), productDetails);
-                  }
-                }
-              } else {
-                retryBillingServiceConnectionWithExponentialBackoff();
-              }
-            });
+          Log.d(LOG_TAG, "Billing Client ready");
+          queryGoogleProducts();
+        } else {
+          Log.e(LOG_TAG, "Billing Setup Failed: " + billingResult.getDebugMessage());
         }
       }
 
-      private void retryBillingServiceConnectionWithExponentialBackoff() {
-        handler.postDelayed(() -> billingClient.startConnection(this), reconnectMilliseconds);
-        reconnectMilliseconds =
-          Math.min(reconnectMilliseconds * 2, RECONNECT_TIMER_MAX_TIME_MILLISECONDS);
+      @Override
+      public void onBillingServiceDisconnected() {
+        Log.w(LOG_TAG, "Billing Service Disconnected, will retry");
+        reconnect();
       }
     });
   }
 
-  private void logBillingResult(@NonNull String location, @NonNull BillingResult billingResult) {
-    final int responseCode = billingResult.getResponseCode();
-    final String debugMessage = billingResult.getDebugMessage();
-    switch (responseCode) {
-      case BillingClient.BillingResponseCode.OK:
-      case BillingClient.BillingResponseCode.USER_CANCELED:
-        Log.d(LOG_TAG, location + ": " + responseCode + "/" + debugMessage);
-        break;
-      case BillingClient.BillingResponseCode.SERVICE_DISCONNECTED:
-      case BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE:
-      case BillingClient.BillingResponseCode.BILLING_UNAVAILABLE:
-      case BillingClient.BillingResponseCode.ITEM_UNAVAILABLE:
-      case BillingClient.BillingResponseCode.DEVELOPER_ERROR:
-      case BillingClient.BillingResponseCode.ERROR:
-        Log.e(LOG_TAG, location + ": " + responseCode + "/" + debugMessage);
-        break;
-      // These response codes are not expected
-      case BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED:
-      case BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED:
-      case BillingClient.BillingResponseCode.ITEM_NOT_OWNED:
-      default:
-        Log.wtf(LOG_TAG, location + ": " + responseCode + "/" + debugMessage);
-    }
+  private void reconnect() {
+    handler.postDelayed(this::initGoogleBillingClient, RECONNECT_TIMER_START_MILLISECONDS);
+  }
+
+  private void queryGoogleProducts() {
+    final List<QueryProductDetailsParams.Product> googleProducts = Arrays.asList(
+      QueryProductDetailsParams.Product.newBuilder()
+        .setProductId(productIds[0])
+        .setProductType(BillingClient.ProductType.INAPP)
+        .build(),
+      QueryProductDetailsParams.Product.newBuilder()
+        .setProductId(productIds[1])
+        .setProductType(BillingClient.ProductType.INAPP)
+        .build(),
+      QueryProductDetailsParams.Product.newBuilder()
+        .setProductId(productIds[2])
+        .setProductType(BillingClient.ProductType.INAPP)
+        .build());
+    final QueryProductDetailsParams queryProductDetailsParams = QueryProductDetailsParams.newBuilder()
+      .setProductList(googleProducts)
+      .build();
+    billingClient.queryProductDetailsAsync(queryProductDetailsParams, (billingResult, productDetailsResult) -> {
+      if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+        final List<ProductDetails> productDetailss = productDetailsResult.getProductDetailsList();
+        for (ProductDetails productDetails : productDetailss) {
+          ownProductDetailss.put(productDetails.getProductId(), productDetails);
+        }
+      } else {
+        Log.e(LOG_TAG, "Query Product Details failed: " + billingResult.getDebugMessage());
+      }
+    });
+  }
+
+  private void handlePurchase(@NonNull Purchase purchase) {
+    final ConsumeParams consumeParams = ConsumeParams.newBuilder().setPurchaseToken(purchase.getPurchaseToken()).build();
+    billingClient.consumeAsync(consumeParams, this);
   }
 }
